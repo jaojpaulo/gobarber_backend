c8da49152ffc335e91a82797142cf1ba9571bf89
jaojpaulo <jaojpaulosantos@gmail.com>
2021-02-16 21:37:49 -0300

    Visualização de agenda do prestador

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,39 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProvideAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProvideAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProvideAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2021, 4, 20, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2021, 4, 20, 5, 0, 0),
+    });
+
+    const availability = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      year: 2021,
+      month: 5,
+      day: 20,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([appointment1, appointment2]),
+    );
+  });
+});
