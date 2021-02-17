61ed6388fae5ae97fdfa61a550d4071f02193a67
jaojpaulo <jaojpaulosantos@gmail.com>
2021-02-17 20:27:40 -0300

    Configurando envio de email pelo AWS SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'etherial' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'etherial',
+
+  defaults: {
+    from: {
+      email: 'contato@devjpsantos.com',
+      name: 'Equipe de contato',
+    },
+  },
+} as IMailConfig;
